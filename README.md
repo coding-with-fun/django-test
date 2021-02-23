69cd248f2ec35ee7d4ac97abb9e607296b32641f
Harsh <harsh2124p@gmail.com>
2021-02-23 22:35:09 +0530

    Created authenticated URL

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # django-test
+
+## Requests
+
+1. API URL -
+```
+curl -X POST -H "Content-Type: application/json" -d '{"username":"testuser", "password1":"testpassword", "password2":"testpassword"}' localhost:8000/registration/
+```
+```
+Response: {"key":"1565c60a136420bc733b10c4a165e07698014acb"}
+```
+
+2. API URL -
+```
+curl -X GET -H 'Authorization: Token 1565c60a136420bc733b10c4a165e07698014acb' localhost:8000/api/welcome
+```
+```
+Response {"message": "Welcome AH_Bookstore App!"}
+```
+
+3. API URL -
+```
+curl -X GET -H '' localhost:8000/api/welcome
+```
+```
+Response {"detail":"Authentication credentials were not provided."}
+```
--- bookstore_app/bookstore_app/settings.py
+++ bookstore_app/bookstore_app/settings.py
@@ -39,7 +39,16 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api',
     'rest_framework',
+    'rest_framework.authtoken',
+    'rest_auth',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'rest_auth.registration',
 ]
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -50,6 +59,19 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    # Authentication Scheme
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    # Permission Policies
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
 
 ROOT_URLCONF = 'bookstore_app.urls'
 
